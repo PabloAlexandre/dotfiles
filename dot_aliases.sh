639bc0dc590d88d8100dfc7a1f5161b18246f239
Pablo Salgado <pablo.salgado@varsitytutors.com>
2025-01-25 15:55:02 -0300

    Initial commit

---
+++ dot_aliases.sh
@@ -0,0 +1,9 @@
+alias ggpushf="ggpush --force-with-lease"
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
+alias cat="bat --paging=never"
+alias catp="bat"
+alias vim="nvim"
+alias cz=chezmoi
+alias tf="terraform"
+alias htop="htop"
+alias top="btop"
